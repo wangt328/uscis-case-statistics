c34921e2768071357e2e1a7ad1b3dc0341f39342
Wang Tian <wangt1017@gmail.com>
2020-11-22 20:17:44 -0500

    Update client.py

--- libs/mongodb/client.py
+++ libs/mongodb/client.py
@@ -8,7 +8,7 @@ class MongoDatabase(object):
     """
   
     def __new__(cls, db: str, *arg, **kwargs) -> pymongo.database.Database:
-        url = 'mongodb+srv://{}:{}@dev.ba3z7.mongodb.net/test?retryWrites=true&w=majority'  # @dev.ba3z7.mongodb.net shoudl be updated to yours accordingly 
+        url = 'mongodb+srv://{}:{}@dev.ba3z7.mongodb.net/test?retryWrites=true&w=majority'  # @dev.ba3z7.mongodb.net should be updated to yours accordingly 
         client = pymongo.MongoClient(
             url.format(config.get('username'), config.get('password'))
         )
