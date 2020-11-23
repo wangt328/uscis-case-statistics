d7acd56c1064e3d5304a954e8663e8c93e38b9c8
Wang Tian <wangt1017@gmail.com>
2020-11-22 20:17:35 -0500

    Update client.py

--- libs/mongodb/client.py
+++ libs/mongodb/client.py
@@ -6,9 +6,9 @@ class MongoDatabase(object):
     """This class returns a database object, not a mongo client. In order to access the client,
     use the syntax: `database.client`
     """
-
+  
     def __new__(cls, db: str, *arg, **kwargs) -> pymongo.database.Database:
-        url = 'mongodb+srv://{}:{}@dev.ba3z7.mongodb.net/test?retryWrites=true&w=majority'
+        url = 'mongodb+srv://{}:{}@dev.ba3z7.mongodb.net/test?retryWrites=true&w=majority'  # @dev.ba3z7.mongodb.net shoudl be updated to yours accordingly 
         client = pymongo.MongoClient(
             url.format(config.get('username'), config.get('password'))
         )
