d5c86ab3cc0a893de67fad1d323903c0144e2d46
wangt <wangt1017@gmail.com>
2020-11-23 23:20:42 -0500

    enable saving locally

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from libs.uscis.client import USCISStatusFetcher
 
+CASE_TYPE = 'I-539'
+SERVICE_CENTER = 'LIN'
+FISCAL_YEAR = '20'
+WORK_DAYS = list(range(280, 300))
+CASE_NUMBERS = list(range(50000, 51000, 2))
+
 
 def main():
-    uscis_fetcher = USCISStatusFetcher(batch_size=1000, save_locally=True)  # batch_size should be smaller than 1000
-    uscis_fetcher.query('I-539', 'LIN', '20', list(range(268, 269)), list(range(50000, 50500, 1)))
+    uscis_fetcher = USCISStatusFetcher(batch_size=500, save_locally=True)
+    uscis_fetcher.query(CASE_TYPE, SERVICE_CENTER, FISCAL_YEAR, WORK_DAYS, CASE_NUMBERS)
 
 
 main()
