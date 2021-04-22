500677b3c6b53d6b8b21e5653292106404ce7af5
wangt328 <wangt1017@gmail.com>
2021-04-22 19:31:27 -0400

    make minor changes

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from libs.utils.decorators import timer
 CASE_TYPE = 'I-539'
 SERVICE_CENTER = 'LIN'
 FISCAL_YEAR = '20'
-WORK_DAYS = list(range(260, 290))
+WORK_DAYS = list(range(281, 282))
 CASE_NUMBERS = list(range(50000, 51200, 1))
 
 
 @timer
 def main():
-    uscis_fetcher = USCISStatusFetcher(batch_size=1000, save_locally=False)
+    uscis_fetcher = USCISStatusFetcher(batch_size=1000, save_locally=True)
     uscis_fetcher.query(CASE_TYPE, SERVICE_CENTER, FISCAL_YEAR, WORK_DAYS, CASE_NUMBERS)
 
 
